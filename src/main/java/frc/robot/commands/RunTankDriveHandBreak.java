285a315c1dd0044d3a88e6e1ebc723229ea72ce6
ChesterParker3201 <Chester.j.parker@gmail.com>
2021-02-03 19:06:46 -0500

    New

---
+++ src/main/java/frc/robot/commands/RunTankDriveHandBreak.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class RunTankDriveHandBreak extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Drive m_drive;
+  
+  public RunTankDriveHandBreak(Drive drive) {
+    m_drive = drive;
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("Switching to manual tank drive!");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    if(m_drive.getLeftJoystickTrigger()){
+      m_drive.tankDrive(0.0, m_drive.getRightJoystickY());
+    }else if(m_drive.getrightJoystickTrigger()){
+      m_drive.tankDrive(m_drive.getLeftJoystickY(), 0.0);
+    }else{
+      m_drive.tankDrive(m_drive.getLeftJoystickY(), m_drive.getRightJoystickY());
+    }
+
+    m_drive.tankDrive(m_drive.getLeftJoystickY(), m_drive.getRightJoystickY());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    if(interrupted){
+      System.out.println("Manual tank drive interupted!");
+    }else{
+      System.out.println("Manual tank drive ending normally.");
+    }
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+}
