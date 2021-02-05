30afddae8393eaa3273c6be67bfee1fa87011ff3
spyalexs@gmail.com <you@example.com>
2021-02-04 21:58:38 -0500

    Removed Unsued Import

--- src/main/java/frc/robot/helper/DriveHandlingSetup/HandlingBase.java
+++ src/main/java/frc/robot/helper/DriveHandlingSetup/HandlingBase.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
